bfeb45ac1e33a7da268fd9bdd0e637a83adbaf1b
IanMcT <ian.mctavish@tldsb.on.ca>
2021-12-10 15:37:06 +0000

    Can determine the maximum population

--- script.js
+++ script.js
@@ -15,17 +15,26 @@ function processContents(contents){
   var output = "";
   var total = 0;
   var runOnce = false;
+  //max population
+  var maxProvince = "";
+  var maxPopulation = 0;
+
   for(line of lines){
     var pop = line.split(",");
 
     console.log(pop[1]);
     if(runOnce){
       if(Number.isInteger(parseInt(pop[1]))){
+        if(parseInt(pop[1])>maxPopulation){
+          maxPopulation = parseInt(pop[1])
+          maxProvince = pop[0];
+        }
+        alert("Max: " + maxProvince + " at " + maxPopulation);
         total += parseInt(pop[1]);
       }
     }
     runOnce = true;
-    alert(pop[0] + " pop: " + pop[1] + " total " + total);
+//    alert(pop[0] + " pop: " + pop[1] + " total " + total);
     output += line + "<br />";
   }
   output += "Canada " + total;
